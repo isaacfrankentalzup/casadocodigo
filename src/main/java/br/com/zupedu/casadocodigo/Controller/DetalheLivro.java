b9ea6e52aebe61bd1154541faa75257946fc73e2
isaacfrankentalzup <isaac.frankental@zup.com.br>
2021-07-04 08:51:49 -0300

    Página detalhe Livro 0.0.7

---
+++ src/main/java/br/com/zupedu/casadocodigo/Controller/DetalheLivro.java
@@ -0,0 +1,44 @@
+package br.com.zupedu.casadocodigo.Controller;
+
+import br.com.zupedu.casadocodigo.Controller.dto.DetalheLivroResponse;
+import br.com.zupedu.casadocodigo.model.Livro;
+import br.com.zupedu.casadocodigo.repository.LivroRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/detalheslivros")
+public class DetalheLivro {
+
+    private LivroRepository repository;
+
+    public DetalheLivro(LivroRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalheLivroResponse> buscarLivroDetalhes(@PathVariable Long id){
+        Optional<Livro>  livroEncontrado = repository.findById(id);
+
+        if(livroEncontrado.isPresent()){
+            Livro livro = livroEncontrado.get();
+            DetalheLivroResponse detalheLivroResponse =
+                    new DetalheLivroResponse(livro.getTitulo(),
+                            livro.getResumo(),
+                            livro.getSumario(),
+                            livro.getPreco(),
+                            livro.getPaginas(),
+                            livro.getIsbn(),
+                            livro.getAutor().getNome(),
+                            livro.getAutor().getDescricao());
+            return ResponseEntity.ok(detalheLivroResponse);
+
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
--- src/main/java/br/com/zupedu/casadocodigo/repository/LivroRepository.java
+++ src/main/java/br/com/zupedu/casadocodigo/repository/LivroRepository.java
@@ -4,4 +4,5 @@ import br.com.zupedu.casadocodigo.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+
 }
